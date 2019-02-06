92fdab1eb52b3ab314ca49643673074c19f0120b
WilsonLo <wilson-power0130@hotmail.com>
2019-02-06 14:10:25 +0800

    策略模式 Concrete

---
+++ Java_Design/src/concrete_strategy/SortVillageById.java
@@ -0,0 +1,15 @@
+package concrete_strategy;
+
+import java.util.Comparator;
+
+//Using ID to sort (Concrete Strategy)
+public class SortVillageById implements Comparator<Village>{
+	
+	@Override
+	public int compare(Village o1, Village o2) {
+		if (o1.id > o2.id) return 1;
+		else if (o1.id < o2.id) return -1;
+		else return 0;
+	}
+	
+}
